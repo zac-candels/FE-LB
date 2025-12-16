c24f250ee95f3d8fca7d03e06ee140fb93a95e57
zcandels <s2767572@ed.ac.uk>
2025-12-16 11:46:56 +0000

    (1) I was re-computing and re-projecting velocity in each timestep (which is wrong). (2) The constants for the chemical potential were wrong.

--- binary/CL_dynamics/main.py
+++ binary/CL_dynamics/main.py
@@ -1,8 +1,8 @@
 import fenics as fe
 import os
 import numpy as np
-#import matplotlib
-#matplotlib.use("TkAgg")
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.tri as tri
 import time 
@@ -13,11 +13,11 @@ plt.close('all')
 
 # Where to save the plots
 WORKDIR = os.getcwd()
-outDirName = os.path.join(WORKDIR, "figures_CA30")
+outDirName = os.path.join(WORKDIR, "figures_speedup")
 os.makedirs(outDirName, exist_ok=True)
 
 T = 1500
-CFL = 0.1
+CFL = 0.2
 initBubbleDiam = 5
 L_x, L_y = 2*initBubbleDiam, 2*initBubbleDiam
 nx, ny = 100, 100
@@ -50,7 +50,6 @@ rho_l = 1#rho_h/100
 eta_h = (Mo * sigma**3 * rho_h)**(1/4)
 eta_l = eta_h/100
 
-
 beta = 12*sigma/eps
 
 kappa = 3*sigma*eps/2 
@@ -59,11 +58,11 @@ kappa = 3*sigma*eps/2
 tau_h = 1 #eta_h / (c_s2 * rho_h * dt )
 tau_l = 1# eta_l / (c_s2 * rho_l * dt )
 
-theta = 60 * np.pi / 180
+theta = 30 * np.pi / 180
 
-M_tilde = 0.005
+M_tilde = 0.01
 
-center_init_x, center_init_y = L_x/2, 0
+center_init_x, center_init_y = L_x/2, initBubbleDiam/2 - 2
 
 Q = 9
 # D2Q9 lattice velocities
@@ -128,7 +127,6 @@ f_nP1 = []
 for idx in range(Q):
     f_nP1.append(fe.Function(V))
 phi_nP1 = fe.Function(V)
-mu_nP1 = fe.Function(V)
 
 # Define FE functions to hold post-collision distributions
 f_star = []
@@ -261,12 +259,13 @@ def body_Force(f_list, phi, mu, vel_idx):
 
 
 # Define Allen-Cahn mobility
+
 def mobility(phi_n):
     grad_phi_n = fe.grad(phi_n)
-
-    abs_grad_phi_n = fe.sqrt(fe.dot(grad_phi_n, grad_phi_n) + 1e-6)
+    
+    abs_grad_phi_n = fe.sqrt(fe.dot(grad_phi_n, grad_phi_n) + 1e-12)
     inv_abs_grad_phi_n = 1.0 / abs_grad_phi_n
-
+    
     mob = M_tilde*( 1 - 4*phi_n*(1 - phi_n)/eps * inv_abs_grad_phi_n )
     return mob
     
@@ -280,17 +279,7 @@ for idx in range(Q):
     f_n[idx] = (fe.project(f_equil_init(idx), V))
     
 # Initialize \phi
-# phi_init_expr = fe.Expression(
-#     "x[1] > -1e-16 ? 0.5 - 0.5 * tanh( 2.0 * (sqrt(pow(x[0]-xc,2) + pow(x[1]-yc,2)) - R) / eps ) : 0",
-#     degree=4,  # polynomial degree used for interpolation
-#     xc=center_init_x,
-#     yc=center_init_y,
-#     R=initBubbleDiam/2,
-#     eps=eps
-# )
-
-# Initialize \phi
-phi_init = phi_init_expr = fe.Expression(
+phi_init_expr = fe.Expression(
     "0.5 - 0.5 * tanh( 2.0 * (sqrt(pow(x[0]-xc,2) + pow(x[1]-yc,2)) - R) / eps )",
     degree=2,  # polynomial degree used for interpolation
     xc=center_init_x,
@@ -425,11 +414,12 @@ bilin_form_mu = f_trial * v * fe.dx
 
 lin_form_AC = phi_n * v * fe.dx - dt*v*fe.dot(vel_n, fe.grad(phi_n))*fe.dx\
     - dt*fe.dot(fe.grad(v), mobility(phi_n)*fe.grad(phi_n))*fe.dx\
-        - 0.5*dt**2 * fe.dot(vel_n, fe.grad(v)) * fe.dot(vel_n, fe.grad(phi_n)) *fe.dx\
+        - 0.5*dt**2 * fe.dot(vel_n, fe.grad(v)) * fe.dot(vel_n, fe.grad(phi_n)) *fe.dx#\
+           # - (1/10)*dt*(np.cos(theta)*np.sqrt(2*kappa*beta)/kappa)*v*mobility(phi_n)*(phi_n - phi_n**2)*ds_bottom
 
 lin_form_mu = 4*beta*(phi_n - 1)*(phi_n - 0)*(phi_n - 0.5)*v*fe.dx\
-    + kappa*fe.dot(fe.grad(phi_n),fe.grad(v))*fe.dx - np.sqrt(2*kappa*beta)/kappa\
-        * np.cos(theta)*(phi_n - phi_n**2)*v*ds_bottom
+    + kappa*fe.dot(fe.grad(phi_n),fe.grad(v))*fe.dx\
+        - kappa*np.cos(theta)*np.sqrt(2*kappa*beta)*v*(phi_n - phi_n**2)*ds_bottom
 
 for idx in range(Q):
 
@@ -478,7 +468,18 @@ for idx in range(Q):
 solver_list = []
 for idx in range(Q):
     A = sys_mat[idx]
-    solver = fe.LUSolver(A)
+
+    # Create CG solver
+    solver = fe.KrylovSolver("cg", "ilu")  # use ILU preconditioner
+    solver.set_operator(A)
+
+    # Optional: set solver parameters
+    prm = solver.parameters
+    prm["absolute_tolerance"] = 1e-12
+    prm["relative_tolerance"] = 1e-8
+    prm["maximum_iterations"] = 1000
+    prm["nonzero_initial_guess"] = False
+
     solver_list.append(solver)
 
 phi_mat = fe.assemble(bilin_form_AC)
@@ -486,29 +487,18 @@ mu_mat = fe.assemble(bilin_form_mu)
 phi_solver = fe.KrylovSolver("cg", "ilu")
 phi_solver.set_operator(phi_mat)
 
-mu_solver = fe.LUSolver("mumps")
-#mu_solver = fe.KrylovSolver("cg", "ilu")
-
+mu_solver = fe.KrylovSolver("cg", "ilu")
 mu_solver.set_operator(mu_mat)
 
 
 rhs_mu = fe.assemble(lin_form_mu)
 
-mu_solver.solve(mu_mat, mu_n.vector(), rhs_mu)
-
-
-fe.plot(mu_n)
+fe.solve(mu_mat, mu_n.vector(), rhs_mu)
 
 # Timestepping
 t = 0.0
 for n in range(num_steps):
     t += dt
-    
-    print("n = ", n)
-    
-    rhs_AC = fe.assemble(lin_form_AC)
-    rhs_mu = fe.assemble(lin_form_mu)
-    
     
     # f_pre_stack = np.array([fi.vector().get_local() for fi in f_n])   # shape (Q,N)
     # rho_pre = np.sum(f_pre_stack, axis=0)
@@ -569,11 +559,12 @@ for n in range(num_steps):
     for idx in range(Q):
         solver_list[idx].solve(f_nP1[idx].vector(), rhs_vec_streaming[idx])
         
+        
+    rhs_AC = fe.assemble(lin_form_AC)
+    rhs_mu = fe.assemble(lin_form_mu)
     
-    
-    #phi_solver.solve(phi_nP1.vector(), rhs_AC)
-    fe.solve(phi_mat, phi_nP1.vector(), rhs_AC)
-    fe.solve(mu_mat, mu_n.vector(), rhs_mu)
+    phi_solver.solve(phi_mat, phi_nP1.vector(), rhs_AC)
+    mu_solver.solve(mu_mat, mu_n.vector(), rhs_mu)
 
 
     # Update previous solutions
@@ -581,10 +572,8 @@ for n in range(num_steps):
     for idx in range(Q):
         f_n[idx].assign(f_nP1[idx])
     phi_n.assign(phi_nP1)
-    vel_expr = vel(f_n)
-    fe.project(vel_expr, V_vec, function=vel_n)
     
-    if n % 100 == 0:  # plot every 10 steps
+    if n % 10 == 0:  # plot every 10 steps
         coords = mesh.coordinates()
         phi_vals = phi_n.compute_vertex_values(mesh)
         triangles = mesh.cells()  # get mesh connectivity
