f1c231748007972b37670e6ea6af88ff994d0fd2
s2767572-ec288 <s2767572-ec288@login02.hsn.cm.system.cirrus.ac.uk>
2025-12-30 18:34:34 +0000

    Modified the program in AllenCahn1D. The program now runs. There is a problem, however. We currently have instability.

--- AllenCahn/AllenCahn1D/AC1D.py
+++ AllenCahn/AllenCahn1D/AC1D.py
@@ -1,13 +1,62 @@
-import numpy as np
 import fenics as fe
+import os
+import numpy as np
+#import matplotlib
+#matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
+import matplotlib.tri as tri
 
-dt = 1e-4
-T = 1
-c1 = np.sqrt(0.0001)
-c2 = 5
+from mpi4py import MPI
+comm = fe.MPI.comm_world
+rank = fe.MPI.rank(comm)
 
-num_steps = int(T/dt)
+plt.close('all')
+
+# Where to save the plots
+WORKDIR = os.getcwd()
+
+def plot_solution_1d(phi, mesh, t, step, outdir):
+    """
+    Plot and save 1D phase-field solution
+    """
+    # Get mesh coordinates and solution values
+    x = mesh.coordinates().flatten()
+    phi_vals = phi.vector().get_local()
+
+    # Sort (important for plotting)
+    idx = np.argsort(x)
+    x = x[idx]
+    phi_vals = phi_vals[idx]
+
+    plt.figure(figsize=(6, 4))
+    plt.plot(x, phi_vals, lw=2)
+    plt.xlabel("x")
+    plt.ylabel(r"$\phi$")
+    plt.title(f"t = {t:.4f}")
+    plt.ylim(-0.5, 1.5)
+    plt.grid(True)
+
+    filename = os.path.join(outdir, f"phi_{step:05d}.png")
+    plt.savefig(filename, dpi=150, bbox_inches="tight")
+    plt.close()
+
+T = 100
+L_x, L_y = 1, 1
+nx, ny = 128, 128
+h = L_x/nx
+dt = 0.1*h
+num_steps = int(np.ceil(T/dt))
+
+M_tilde = 0.005
+
+outDirName = f"1D"
+
+if rank == 0:
+    os.makedirs(outDirName, exist_ok=True)
+comm.Barrier()
+
+
+eps = 0.7*h
 
 class PeriodicBoundary(fe.SubDomain):
     # Left boundary is "target"
@@ -27,54 +76,104 @@ mesh = fe.IntervalMesh(100, -1, 1)
 
 V = fe.FunctionSpace(mesh, "CG", 1, constrained_domain=pbc)
 
+# Define trial and test functions, as well as
+# finite element functions at previous timesteps
 
-phi_nP1 = fe.Function(V)
+f_trial = fe.TrialFunction(V)
+
+phi_n = fe.Function(V)
+mu_nP1 = fe.Function(V)
+
 v = fe.TestFunction(V)
 
-phi_init_expr = fe.Expression("pow(x[0],2)  * cos(pi * x[0])",
-                              pi = 3.14159, degree = 2)
-
-phi_n = fe.interpolate(phi_init_expr, V)
+phi_nP1 = fe.Function(V)
 
 
-Functional = (phi_nP1 - phi_n)*v*fe.dx + dt*c1**2 * fe.dot( fe.grad(phi_nP1), fe.grad(v) )*fe.dx\
-    + dt*c2*( phi_nP1**3 - phi_nP1 )*v*fe.dx
-    
+# Define Allen-Cahn mobility
+def mobility(phi_n):
+    grad_phi_n = fe.grad(phi_n)
 
-J = fe.derivative(Functional, phi_nP1, fe.TrialFunction(V))
+    abs_grad_phi_n = fe.sqrt(fe.dot(grad_phi_n, grad_phi_n) + 1e-6)
+    inv_abs_grad_phi_n = 1.0 / abs_grad_phi_n
 
-
-
-problem = fe.NonlinearVariationalProblem(Functional, phi_nP1, bcs=None, J=J)
-solver  = fe.NonlinearVariationalSolver(problem)
-prm = solver.parameters
-# optional: configure Newton tolerances
-prm['newton_solver']['relative_tolerance'] = 1e-8
-prm['newton_solver']['maximum_iterations'] = 25
-prm['newton_solver']['linear_solver'] = 'mumps'  # or 'lu' / 'cg' + precond
+    mob = M_tilde*( 1 - 4*phi_n*(1 - phi_n)/eps * inv_abs_grad_phi_n )
+    return mob
 
 
-t = 0
+# Initialize \phi
+phi_init_expr = fe.Expression(
+    "0.5 * (1.0 + tanh((x[0] - x0)/(sqrt(2)*eps)))",
+    degree=4,
+    eps=eps,
+    x0=0.0
+)
+
+
+phi_n = fe.project(phi_init_expr, V)
+
+bilin_form_AC = f_trial * v * fe.dx
+
+lin_form_AC = phi_n * v * fe.dx - dt*fe.dot(fe.grad(v), mobility(phi_n)*fe.grad(phi_n))*fe.dx
+
+
+phi_mat = fe.assemble(bilin_form_AC)
+
+phi_solver = fe.KrylovSolver("cg", "hypre_amg")
+phi_solver.set_operator(phi_mat)
+prm = phi_solver.parameters
+prm["absolute_tolerance"] = 1e-14
+prm["relative_tolerance"] = 1e-8
+prm["maximum_iterations"] = 1000
+prm["nonzero_initial_guess"] = False
+
+outfile = fe.XDMFFile(comm, f"{outDirName}/solution.xdmf")
+outfile.parameters["flush_output"] = True
+outfile.parameters["functions_share_mesh"] = True
+outfile.parameters["rewrite_function_mesh"] = False
+
+# Timestepping
+t = 0.0
 for n in range(num_steps):
-    
     t += dt
     
-    solver.solve()
+    if n % 10 == 0:  # plot every 10 steps
     
+        #c_n.rename("phi", "phase field")  # Optional: label for visualization
+        #xdmf_file.write(c_n, t)
+        
+        outfile.write(phi_n, t)
+
+        if rank == 0:
+            plot_solution_1d(phi_n, mesh, t, n, outDirName)
+            
+    
+    rhs_AC = fe.assemble(lin_form_AC)
+    
+    phi_solver.solve(phi_nP1.vector(), rhs_AC)
+
+
+    # Update previous solutions
+
     phi_n.assign(phi_nP1)
+            
     
-    if abs(t - 0.75) < dt:
-        plt.figure
-        fe.plot(phi_nP1)
-        title_str = f"t= {t}"
-        plt.title(title_str)
-        
-        
-        a = 1
+    a = 1    
+
+
     
     
+
+
+
+
+
+
+
+
+
     
     
+
 
 
 
--- binary/CL_dynamics/1d.py
+++ binary/CL_dynamics/1d.py
@@ -30,26 +30,10 @@ num_steps = int(np.ceil(T/dt))
 g = 0.0981
 sigma = 0.005 #0.1
 
-# Lattice speed of sound
-c_s = np.sqrt(1/3)
-c_s2 = 1/3
-
-# Bond number
-Bo = 100
-
-# Morton number
-Mo = 1000
-
 # Cahn number
 Cn = 0.05
 
 eps = Cn * initBubbleDiam
-
-rho_h = 1#sigma*Bo*g / initBubbleDiam**2
-rho_l = 1#rho_h/100
-
-eta_h = (Mo * sigma**3 * rho_h)**(1/4)
-eta_l = eta_h/100
 
 beta = 12*sigma/eps
 
@@ -63,33 +47,12 @@ theta_deg = 30
 theta = theta_deg * np.pi / 180
 
 WORKDIR = os.getcwd()
-outDirName = os.path.join(WORKDIR, f"figures_CA{theta_deg}")
+outDirName = os.path.join(WORKDIR, f"1D")
 os.makedirs(outDirName, exist_ok=True)
 
 M_tilde = 0.01
 
 center_init_x, center_init_y = L_x/2, initBubbleDiam/2 - 2
-
-Q = 9
-# D2Q9 lattice velocities
-xi = [
-    fe.Constant((0.0,  0.0)),
-    fe.Constant((1.0,  0.0)),
-    fe.Constant((0.0,  1.0)),
-    fe.Constant((-1.0,  0.0)),
-    fe.Constant((0.0, -1.0)),
-    fe.Constant((1.0,  1.0)),
-    fe.Constant((-1.0,  1.0)),
-    fe.Constant((-1.0, -1.0)),
-    fe.Constant((1.0, -1.0)),
-]
-
-# Corresponding weights
-w = np.array([
-    4/9,
-    1/9, 1/9, 1/9, 1/9,
-    1/36, 1/36, 1/36, 1/36
-])
 
 # Set up domain. For simplicity, do unit square mesh.
 
@@ -128,140 +91,6 @@ mu_n = fe.Function(V)
 
 v = fe.TestFunction(V)
 
-# Define FE functions to hold post-streaming solution at nP1 timesteps
-f_nP1 = []
-for idx in range(Q):
-    f_nP1.append(fe.Function(V))
-phi_nP1 = fe.Function(V)
-
-# Define FE functions to hold post-collision distributions
-f_star = []
-for idx in range(Q):
-    f_star.append(fe.Function(V))
-
-
-# Define density
-def rho(phi):
-    return rho_h*phi + rho_l*(1 - phi)
-
-def tau_fn(phi):
-    return phi*tau_h + (1 - phi)*tau_l
-
-# Define dynamic pressure
-def get_pBar(f_list):
-    return f_list[0] + f_list[1] + f_list[2] + f_list[3] + f_list[4]\
-        + f_list[5] + f_list[6] + f_list[7] + f_list[8]
-
-# Define velocity
-
-def vel(f_list):
-    distr_fn_sum = f_list[0]*xi[0] + f_list[1]*xi[1] + f_list[2]*xi[2]\
-        + f_list[3]*xi[3] + f_list[4]*xi[4] + f_list[5]*xi[5]\
-        + f_list[6]*xi[6] + f_list[7]*xi[7] + f_list[8]*xi[8]
-
-    velocity = distr_fn_sum/c_s**2
-
-    return velocity
-
-
-# Define initial equilibrium distributions
-def f_equil_init(vel_idx):
-    
-    # We'll take \bar{p} := 1.0
-    return w[vel_idx] 
-
-
-# Define equilibrium distribution
-# def f_equil(f_list, vel_idx):
-#     dyn_pres_expr = dyn_pres(f_list)
-#     u_expr = vel(f_list)
-#     ci = xi[vel_idx]
-#     ci_dot_u = fe.dot(ci, u_expr)
-#     return w[vel_idx] * (
-#         dyn_pres_expr
-#         + ci_dot_u
-#         + ci_dot_u**2 / (2*c_s**2)
-#         - fe.dot(u_expr, u_expr) / 2
-#     )
-
-xi_array = np.array([[float(c.values()[0]), float(c.values()[1])] for c in xi])
-
-def f_equil(f_list, idx):
-    """
-    Compute equilibrium distribution for direction idx
-    Returns a NumPy array (values at all DoFs).
-    """
-    # Number of DoFs
-    N = f_list[0].vector().size()
-
-    # Stack all f_i values: shape (Q, N)
-    f_stack = np.array([f.vector().get_local() for f in f_list])
-
-    # Compute density at each DoF
-    p_bar = np.sum(f_stack, axis=0)  # shape (N,)
-
-    # Compute velocity at each DoF
-    ux_vec = np.sum(f_stack * xi_array[:,0][:,None], axis=0) / c_s**2
-    uy_vec = np.sum(f_stack * xi_array[:,1][:,None], axis=0) / c_s**2
-
-    u2 = ux_vec**2 + uy_vec**2
-
-    # Compute ci . u for this direction
-    cu = xi_array[idx,0]*ux_vec + xi_array[idx,1]*uy_vec
-    
-    feq = w[idx]*( p_bar + cu + cu**2/(2*c_s**2) - u2/2)
-
-    return feq  # NumPy array
-
-
-# Define \Gamma
-def Gamma_vel(vel_arg, vel_idx):
-    ci = xi[vel_idx]
-    ci_dot_u = fe.dot(ci, vel_arg)
-    return w[vel_idx] * (
-        1
-        + ci_dot_u / (c_s**2)
-        + ci_dot_u**2 / (2*c_s**4)
-        - fe.dot(vel_arg, vel_arg) / (2*c_s**2)
-    )    
-    
-def Gamma0(vel_idx):
-    return w[vel_idx]
-    
-
-def body_Force(f_list, phi, mu, vel_idx):
-    
-    grav = fe.Constant((0.0, 9.81))
-    p_bar = get_pBar(f_list)
-    fluid_vel = vel(f_list)
-    density = rho(phi)
-    p = density * p_bar 
-    buoyancy = - grav*(density - rho_h)
-    tau = tau_fn(phi)
-    
-    eta = c_s**2 * density * tau * dt
-    
-    p_bar_grad = fe.grad(p_bar)
-    p_grad = fe.grad(p)
-    phi_grad = fe.grad(phi)
-    density_grad = fe.grad(density)
-    
-    sym_grad_u = 2 * fe.sym(fe.grad(fluid_vel))
-    
-    term1 = -Gamma_vel(fluid_vel, vel_idx)\
-        *fe.dot( (xi[vel_idx] - fluid_vel),  p_grad/density ) 
-    
-    term2 = Gamma0(vel_idx)*fe.dot( (xi[vel_idx] - fluid_vel), p_bar_grad )
-    
-    term3 = Gamma_vel(fluid_vel, vel_idx)\
-        *fe.dot( (xi[vel_idx] - fluid_vel), phi_grad*mu/density )
-        
-    term4 = Gamma_vel(fluid_vel, vel_idx)\
-        *fe.dot( (xi[vel_idx] - fluid_vel),  sym_grad_u* density_grad )\
-            * (eta/density**2)
-    
-
-    return term1 + term2 + term3 + term4
 
 
 # Define Allen-Cahn mobility
@@ -316,89 +145,6 @@ plt.show()
 #plt.close()
 
 
-
-# Define boundary conditions.
-
-# For f_5, f_2, and f_6, equilibrium boundary conditions at lower wall
-# Since we are applying equilibrium boundary conditions
-# and assuming no slip on solid walls, f_i^{eq} reduces to
-# \rho * w_i
-
-tol = 1e-8
-
-
-def Bdy_Lower(x, on_boundary):
-    if on_boundary:
-        if fe.near(x[1], 0, tol):
-            return True
-        else:
-            return False
-    else:
-        return False
-
-
-rho_expr = sum(fk for fk in f_n)
-
-f5_lower = f_n[7]  # rho_expr
-f2_lower = f_n[4]  # rho_expr
-f6_lower = f_n[8]  # rho_expr
-
-f5_lower_func = fe.Function(V)
-f2_lower_func = fe.Function(V)
-f6_lower_func = fe.Function(V)
-
-fe.project(f5_lower, V, function=f5_lower_func)
-fe.project(f2_lower, V, function=f2_lower_func)
-fe.project(f6_lower, V, function=f6_lower_func)
-
-bc_f5 = fe.DirichletBC(V, f5_lower_func, Bdy_Lower)
-bc_f2 = fe.DirichletBC(V, f2_lower_func, Bdy_Lower)
-bc_f6 = fe.DirichletBC(V, f6_lower_func, Bdy_Lower)
-
-# Similarly, we will define boundary conditions for f_7, f_4, and f_8
-# at the upper wall. Once again, boundary conditions simply reduce
-# to \rho * w_i
-
-
-tol = 1e-8
-
-
-def Bdy_Upper(x, on_boundary):
-    if on_boundary:
-        if fe.near(x[1], L_y, tol):
-            return True
-        else:
-            return False
-    else:
-        return False
-
-
-rho_expr = sum(fk for fk in f_n)
-
-f7_upper = f_n[5]  # rho_expr
-f4_upper = f_n[2]  # rho_expr
-f8_upper = f_n[6]  # rho_expr
-
-f7_upper_func = fe.Function(V)
-f4_upper_func = fe.Function(V)
-f8_upper_func = fe.Function(V)
-
-fe.project(f7_upper, V, function=f7_upper_func)
-fe.project(f4_upper, V, function=f4_upper_func)
-fe.project(f8_upper, V, function=f8_upper_func)
-
-bc_f7 = fe.DirichletBC(V, f7_upper_func, Bdy_Upper)
-bc_f4 = fe.DirichletBC(V, f4_upper_func, Bdy_Upper)
-bc_f8 = fe.DirichletBC(V, f8_upper_func, Bdy_Upper)
-
-# Define variational problems
-
-bilinear_forms_stream = []
-linear_forms_stream = []
-
-bilinear_forms_collision = []
-linear_forms_collision = []
-
 n = fe.FacetNormal(mesh)
 opp_idx = {0: 0, 1: 3, 2: 4, 3: 1, 4: 2, 5: 7, 6: 8, 7: 5, 8: 6}
 
@@ -427,40 +173,6 @@ lin_form_mu = 4*beta*(phi_n - 1)*(phi_n - 0)*(phi_n - 0.5)*v*fe.dx\
     + kappa*fe.dot(fe.grad(phi_n),fe.grad(v))*fe.dx\
         #- (1/kappa)*np.cos(theta)*np.sqrt(2*kappa*beta)*v*(phi_n - phi_n**2)*ds_bottom
 
-for idx in range(Q):
-
-    bilinear_forms_stream.append(f_trial * v * fe.dx)
-
-    double_dot_product_term = -0.5*dt**2 * fe.dot(xi[idx], fe.grad(f_star[idx]))\
-        * fe.dot(xi[idx], fe.grad(v)) * fe.dx
-
-    dot_product_force_term = 0.5*dt**2 * fe.dot(xi[idx], fe.grad(v))\
-        * body_Force(f_star, phi_n, mu_n, idx) * fe.dx
-        
-
-    if idx in opp_idx:
-        # UFL scalar: dot product with facet normal
-        dot_xi_n = fe.dot(xi[idx], n)
-
-        # indicator = 1.0 when dot_xi_n < 0 (incoming), else 0.0
-        indicator = fe.conditional(fe.lt(dot_xi_n, 0.0),
-                                   fe.Constant(1.0),
-                                   fe.Constant(0.0))
-
-        # build surface term only for incoming distributions
-        surface_term = 0.5*dt**2 * v * fe.dot(xi[idx], fe.grad(f_n[opp_idx[idx]])) \
-            * dot_xi_n * indicator * fe.ds
-    else:
-        # no surface contribution for this idx
-        surface_term = fe.Constant(0.0) * v * fe.ds
-
-    lin_form_idx = f_star[idx]*v*fe.dx\
-        - dt*v*fe.dot(xi[idx], fe.grad(f_star[idx]))*fe.dx\
-        + dt*v*body_Force(f_star, phi_n, mu_n, idx)*fe.dx\
-        + double_dot_product_term\
-        + dot_product_force_term + surface_term
-
-    linear_forms_stream.append(lin_form_idx)
 
 # Assemble matrices for first step
 
@@ -514,63 +226,7 @@ for n in range(num_steps):
     print("n = ", n)
     
     rhs_AC = fe.assemble(lin_form_AC)
-    rhs_mu = fe.assemble(lin_form_mu)
     
-    # f_pre_stack = np.array([fi.vector().get_local() for fi in f_n])   # shape (Q,N)
-    # rho_pre = np.sum(f_pre_stack, axis=0)
-    # momx_pre = np.sum(f_pre_stack * xi_array[:, 0][:, None], axis=0)
-    # momy_pre = np.sum(f_pre_stack * xi_array[:, 1][:, None], axis=0)
-        
-    # f_post_stack = np.zeros_like(f_pre_stack)
-    # Perform collision, get post-collision distributions f_i^*
-    for idx in range(Q):
-        f_eq_vec = f_equil(f_n, idx)
-        #f_eq_vec = f_eq.vector().get_local()
-        f_n_vec = f_n[idx].vector().get_local()
-        
-        phi_vec = phi_n.vector().get_local()
-        
-        tau_vec = phi_vec*tau_h + (1 - phi_vec)*tau_l
-        
-        f_new = f_n_vec - 1/(tau_vec+0.5) * (f_n_vec - f_eq_vec)
-    
-        # f_post_stack[idx, :] = f_new
-        f_star[idx].vector().set_local(f_new)
-        f_star[idx].vector().apply("insert")
-        
-    # rho_post = np.sum(f_post_stack, axis=0)
-    # momx_post = np.sum(f_post_stack * xi_array[:, 0][:, None], axis=0)
-    # momy_post = np.sum(f_post_stack * xi_array[:, 1][:, None], axis=0)
-    
-    # # ---- Compare ----
-    # rho_diff = rho_post - rho_pre
-    # momx_diff = momx_post - momx_pre
-    # momy_diff = momy_post - momy_pre
-    # print("max |drho|   =", np.max(np.abs(rho_diff)))
-    # print("max |d_momentum_x|=", np.max(np.abs(momx_diff)))
-    # print("max |d_momentum_y|=", np.max(np.abs(momy_diff)))
-
-    # Assemble RHS vectors
-    for idx in range(Q):
-        rhs_vec_streaming[idx] = (fe.assemble(linear_forms_stream[idx]))
-
-    f5_lower_func.vector()[:] = f_n[7].vector()[:]
-    f2_lower_func.vector()[:] = f_n[4].vector()[:]
-    f6_lower_func.vector()[:] = f_n[8].vector()[:]
-    f7_upper_func.vector()[:] = f_n[5].vector()[:]
-    f4_upper_func.vector()[:] = f_n[2].vector()[:]
-    f8_upper_func.vector()[:] = f_n[6].vector()[:]
-
-    # Apply BCs for distribution functions 5, 2, and 6
-    bc_f5.apply(sys_mat[5], rhs_vec_streaming[5])
-    bc_f2.apply(sys_mat[2], rhs_vec_streaming[2])
-    bc_f6.apply(sys_mat[6], rhs_vec_streaming[6])
-
-    # Apply BCs for distribution functions 7, 4, 8
-    bc_f7.apply(sys_mat[7], rhs_vec_streaming[7])
-    bc_f4.apply(sys_mat[4], rhs_vec_streaming[4])
-    bc_f8.apply(sys_mat[8], rhs_vec_streaming[8])
-
     # Solve linear system in each timestep, get f^{n+1}
     for idx in range(Q):
         solver_list[idx].solve(f_nP1[idx].vector(), rhs_vec_streaming[idx])
